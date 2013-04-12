00c46cfa704c9f301363c7705993ac4f683d0a3e
hanleybrand <phanley@temple.edu>
2013-04-12 18:01:55 -0400

    rewrite + accounts now created via ldap if they don't exist already

--- mdid-sso/middleware.py
+++ mdid-sso/middleware.py
@@ -1,19 +1,112 @@
 """
-Single Sign On Middleware
+Single Sign On Middleware fork for mdid3
 """
+import sys
 import string
 import time
 from django.http import HttpResponseRedirect
 from django.conf import settings
-from rooibos.auth import login
-# rooibos.auth.ldapauth import LdapAuthenticationBackend as ldap_get
-from rooibos.auth.baseauth import BaseAuthenticationBackend as Base_auth
+from rooibos.auth import login as login
+from rooibos.auth.ldapauth import LdapAuthenticationBackend
 from django.contrib.auth.models import User as User
 from django.contrib.sites.models import Site
 import ldap
+from random import Random
 import logging
-
 from util import generate_sso_token
+
+
+class rooibos_LDAP(LdapAuthenticationBackend):
+    @classmethod
+    def ldap_user_lookup(cls, token_id):
+        """
+        lookup LDAP user and return the attributes needed for account creation
+        This method is called by rooibos_LDAP.new_account_from_ldap()
+        :param token_id (username from sso token)
+        :return: None or user attributes
+        """
+        logging.debug('SSO: create new account for %s in %s' % (token_id, cls))
+        try:
+            for ldap_auth in settings.LDAP_AUTH:
+                username_lookup = token_id.strip()
+                l = ldap.initialize(ldap_auth['uri'])
+                l.protocol_version = ldap_auth['version']
+                for option, value in ldap_auth['options'].iteritems():
+                    l.set_option(getattr(ldap, option), value)
+
+                # for cases where a bind user is needed (most places)
+                if ldap_auth.get('bind_user'):
+                #bind credential for lookup
+                    l.simple_bind(ldap_auth['bind_user'],
+                                  ldap_auth.get('bind_password'))
+                    #search for user to confirm
+                result = l.search_s(ldap_auth['base'],
+                                    ldap_auth['scope'], '%s=%s' % (ldap_auth['cn'], username_lookup),
+                                    ldap_auth.get('attributes', 'attributes'))
+                logging.debug('SSO.new_ldap: search result for %s:  %s? ' % (username_lookup, result,))
+                # check that result isn't empty
+                if len(result) != 1:
+                    logging.debug('SSO: ldap search for %s failed, returned: %s' % (username_lookup, result))
+                    return None
+                elif len(result) != 1:
+                    continue
+
+                # TODO: is result[0][1] specific to the tested implementation? Can it be made more generic?
+                attributes = result[0][1]
+
+                # conform attributes to expected format
+                for attr in ldap_auth['attributes']:
+                    if attr in attributes:
+                        if not type(attributes[attr]) in (tuple, list):
+                            attributes[attr] = (attributes[attr],)
+                    else:
+                        attributes[attr] = []
+                logging.debug('SSO: ldap_user_lookup returning %s' % attributes)
+                return attributes
+        except Exception, e:
+            logging.debug('SSO: ldap_user_lookup for %s failed, returned: %s' % (token_id, e))
+            return None
+
+    @classmethod
+    def new_account_from_ldap(cls, token_id):
+        """
+        Create a new account from username_lookup using LDAP information
+        :param token_id: username_lookup from sso token
+        :return: User object or None
+        """
+        attributes = cls.ldap_user_lookup(token_id)
+
+        for ldap_auth in settings.LDAP_AUTH:
+            try:
+                user = User.objects.get(username=token_id)
+                if user:
+                    return user
+
+            except User.DoesNotExist:
+                # logging.debug('SSO: backend: %s %s ' % (backend, ldap_get))
+                try:
+                    # this password is meant to never be used, so it will be something like
+                    # 'AToWJ ZLDI\x0cUxiRhaDVYsHyN26jwmPfIQtHXbFSA'
+                    password = ' '.join(Random().sample(string.letters +
+                                                        string.digits +
+                                                        string.uppercase +
+                                                        string.whitespace, 40))
+
+                    user = User(username=token_id, password=password)
+                    user.first_name = ' '.join(attributes[ldap_auth['firstname']])
+                    user.last_name = ' '.join(attributes[ldap_auth['lastname']])
+                    user.email = attributes[ldap_auth['email']][0]
+                    user.save()
+                    logging.debug('SSO: creating user %s (%s %s (%s) from LDAP' % (user.id,
+                                                                                   user.first_name,
+                                                                                   user.last_name,
+                                                                                   user.email))
+                    return user
+
+                except:
+                    logging.debug('SSO: new_account_from_ldap failed due to', sys.exc_info()[0])
+                    raise
+            return None
 
 
 class SingleSignOnMiddleware(object):
@@ -33,33 +126,36 @@ class SingleSignOnMiddleware(object):
         except:
             self.protocol = 'http://'
 
+    def redirect_to_top(self):
+        #redirect_to = settings.LOGIN_REDIRECT_URL
+        return HttpResponseRedirect(settings.LOGIN_REDIRECT_URL)
+
     def process_request(self, request):
+        # SESSION_KEY = '_auth_user_id'
+        # BACKEND_SESSION_KEY = '_auth_user_backend'
         token = request.GET.get('token', False)
         token_id = request.GET.get('id', False)
         timestamp = request.GET.get('timestamp', False)
         if token and token_id and timestamp:
-            logging.debug(
-                'SSO: user %s login attempt via SSO in with timestamp %s and token %s \n' % (
-                    token_id, timestamp, token))
+            # logging.debug(
+            #     'SSO: user %s login attempt via SSO in with timestamp %s and token %s \n' % (
+            #         token_id, timestamp, token))
             if self.check_token(token, token_id, timestamp):
                 # everything passed, authenticate user
-                logging.debug('SSO: user %s token and timestamp pass \n' % token_id)
-                #
-                logging.debug('SSO: Attempting to authenticate as %s \n' % token_id)
-
+                #logging.debug('SSO: user %s token and timestamp pass \n' % token_id)
+                #logging.debug('SSO: Attempting to authenticate as %s \n' % token_id)
                 user = self.authenticate(token_id)
-                try:
-                    login(request, user)
-                except NotImplementedError:
-                    logging.info('SSO: ==+== login fail - NotImplementedError - redirecting to login page===== \n')
-                    raise
-                except:
-                    logging.info('SSO: ====== login fail %s redirect to login page================ ' % user)
-                    redirect_to = settings.LOGIN_REDIRECT_URL
-                    return HttpResponseRedirect(redirect_to)
-            else:
-                logging.debug('SSO: user %s login fail with token %s' % (token_id, token, ))
-        return None
+                if user.username == token_id:
+                    try:
+                        # THIS WAS THE KEY TO IT WORKING
+                        user.backend = settings.SSO_BACKEND
+                        login(request, user)
+                        logging.debug('SSO: process_request - user.backend = %s' % user.backend)
+                        #logging.debug(user.last_login)
+                        # logging.debug(request.session['_auth_user_id'])
+                        # logging.debug(request.session['_auth_user_backend'])
+                    except Exception:
+                        raise
 
     def check_token(self, token, token_id, timestamp):
         """
@@ -68,7 +164,7 @@ class SingleSignOnMiddleware(object):
         toke_check = generate_sso_token(token_id, timestamp)
 
         if time.time() - float(timestamp) <= self.timeout:
-            logging.debug('SSO: checking login token \n\t%s \n\tvs check token \n\t%s \n' % (token, toke_check))
+            #logging.debug('SSO: checking login token \n\t%s \n\tvs check token \n\t%s \n' % (token, toke_check))
             return token == toke_check
         else:
             logging.debug('SSO: timestamp %s is out of range with system time (%s) \n' % (timestamp, time.time()))
@@ -82,31 +178,24 @@ class SingleSignOnMiddleware(object):
         """
         from django.contrib.auth import get_backends
 
+        user = None
+
         for backend in get_backends():
-            try:
-                #user = backend.get_user(token_id)
-                #logging.debug(backend.get_user(token_id))
-                logging.debug('SSO.authenticate: username %s ' % token_id)
-                user = User.objects.get(username=token_id)
+            if backend:
+                try:
+                    user = User.objects.get(username=token_id)
+                    #logging.debug('SSO.authenticate: username %s authenticated with %s' % (token_id, backend))
 
-            except User.DoesNotExist:
-                logging.debug('SSO: username %s does not exist in %s' % (token_id, backend.__class__.__name__))
-                user = self.new_account_from_ldap(token_id)
-                return user
-                # except:
-            #     # didn't work, try the next one.
-            #     logging.debug('backend %s - %s' % (backend.__module__, backend.__class__.__name__))
-            #     continue
+                except User.DoesNotExist:
+                    logging.debug('SSO: username %s does not exist in %s' % (token_id, backend.__class__.__name__))
+                    try:
+                        user = rooibos_LDAP.new_account_from_ldap(token_id=token_id)
+                    except Exception, e:
+                        logging.debug('SSO.authenticate: '
+                                      'SSO.rooibos_LDAP.new_account_from_ldap failed for %s ...\n %s' % (token_id, e))
 
-            if user.username != token_id:
-                print 'SSO: %s != %s' % (user.username, token_id)
-                user = self.new_account_from_ldap(token_id)
-                continue
-            # Annotate the user object with the path of the backend.
-            else:
-                user.backend = "%s.%s" % (backend.__module__, backend.__class__.__name__)
-                #logging.debug('user.backend: %s' % user.backend)
-                return user
+        logging.debug('SSO.authenticate: returning user %s ' % user)
+        return user
 
     def process_response(self, request, response):
         """ takes the response output and replaces urls """
@@ -128,7 +217,7 @@ class SingleSignOnMiddleware(object):
 
     def replace_domain_urls(self, content, domains):
         """
-        Replaces urls for domains specified and replaces them with 
+        Replaces urls for domains specified and replaces them with
         a url to the sso view that will generate a token and redirect
         """
         current_domain = Site.objects.get_current().domain
@@ -141,62 +230,3 @@ class SingleSignOnMiddleware(object):
                 domain
             ))
         return content
-
-    def new_account_from_ldap(self, token_id):
-        """
-        Create a new account from username_lookup using LDAP information
-        :param token_id: username_lookup from sso token
-        :return: User object or None
-        """
-
-        for backend in settings.AUTHENTICATION_BACKENDS:
-
-            if backend == 'rooibos.auth.ldapauth.LdapAuthenticationBackend':
-                logging.debug('SSO: create new account for %s in %s' % (token_id, backend))
-                try:
-                    for ldap_auth in settings.LDAP_AUTH:
-                        #ldap_get.authenticate(id,password=None)
-                        username_lookup = token_id.strip()
-                        l = ldap.initialize(ldap_auth['uri'])
-                        l.protocol_version = ldap_auth['version']
-                        for option, value in ldap_auth['options'].iteritems():
-                            l.set_option(getattr(ldap, option), value)
-
-                        if ldap_auth.get('bind_user'):
-                        #bind credential for lookup
-                            l.simple_bind(ldap_auth['bind_user'],
-                                          ldap_auth.get('bind_password'))
-                            #search for user to confirm
-                        result = l.search_s(ldap_auth['base'],
-                                            ldap_auth['scope'], '%s=%s' % (ldap_auth['cn'], username_lookup),
-                                            ldap_auth.get('attributes', 'attributes'))
-                        logging.debug('SSO.new_ldap: search result for %s:  %s? ' % (username_lookup, result,))
-                        if len(result) != 1:
-                            logging.debug('SSO: ldap search for %s failed, returned: %s' % (username_lookup, result))
-                            return None
-                        elif len(result) != 1:
-                            continue
-                        attributes = result[0][1]
-                        for attr in ldap_auth['attributes']:
-                            if attributes.has_key(attr):
-                                if not type(attributes[attr]) in (tuple, list):
-                                    attributes[attr] = (attributes[attr],)
-                            else:
-                                attributes[attr] = []
-                        try:
-                            user = User.objects.get(username=username_lookup)
-                        except User.DoesNotExist:
-                            user = backend._create_user(backend, username_lookup,
-                                                        None,
-                                                        ' '.join(attributes[ldap_auth['firstname']]),
-                                                        ' '.join(attributes[ldap_auth['lastname']]),
-                                                        attributes[ldap_auth['email']][0])
-                        return user
-                except:  # this shouldn't be so vague, but I'm not sure what to check for
-                    if username_lookup and backend:
-                        logging.debug('SSO: User %s was not able to be created from LDAP (%s). What now?' % (
-                            username_lookup, backend))
-                    raise
-
-        logging.debug('SSO: new_account_from_ldap for %s failed' % token_id)
-        return None
--- settings_sso.py
+++ settings_sso.py
@@ -11,4 +11,9 @@ SSO_DOMAINS = ''
 # this should be changed prior to deployment
 # https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SSO_SECRET = 'zp*+x2)p6q)ft6han8rn8717d#h#2hk$4s-2f*8n*1fw&04h+j'
+
+
 SSO_CAPS = True
+
+# currently this only supports LDAP, but hopefully in the future others will be supported
+SSO_BACKEND = 'rooibos.auth.ldapauth.LdapAuthenticationBackend'
